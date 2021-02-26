365669e9d6f9fb2cb88320a516d11bad83fe79fd
sistiri <sistiri@yahoo.com>
2021-02-26 01:27:51 +0100

    EditOrderComponent updated

--- src/app/page/edit-order/edit-order.component.ts
+++ src/app/page/edit-order/edit-order.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Order } from 'src/app/model/order';
+import { OrderService } from 'src/app/service/order.service';
 
 @Component({
   selector: 'app-edit-order',
@@ -7,7 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditOrderComponent implements OnInit {
 
-  constructor() { }
+  order: Order = new Order();
+
+  constructor(
+    private orderService: OrderService,
+    private rout: ActivatedRoute,
+  ) {
+    this.rout.params.subscribe( params => {
+      this.orderService.get(params.id).forEach( order => {
+        this.order = order;
+      })
+    })
+
+   }
 
   ngOnInit(): void {
   }
