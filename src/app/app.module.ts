b69720b1af1b650d40470ec09fce705fbc612ea3
mllaci01 <mllaci078@gmail.com>
2021-03-04 17:40:28 +0100

    bill order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -87,6 +87,7 @@ import {NgxPaginationModule} from 'ngx-pagination';
     AngularFirestoreModule,
 
     NgxPaginationModule,
+    ReactiveFormsModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: InterceptorService, multi: true }
